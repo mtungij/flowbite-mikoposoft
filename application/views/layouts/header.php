758a878b6d02f27fcd1800ddff1abb0e85756599
mtungij <jamesmtungi2013@gmail.com>
2025-02-22 10:01:10 +0300

    hallow flowbite

---
+++ application/views/layouts/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/public/output.css" rel="stylesheet">
+</head>
+<body class="h-dvh overflow-auto">
